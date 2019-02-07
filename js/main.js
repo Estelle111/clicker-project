d9bbd42f757483f047a818c3c4c4abfd9423a1f0
Amondt <mondt.alexande>
2019-02-07 12:32:15 +0100

    add fct comment

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ window.onload = () => {
         this.evolLevel = function(level){
             return level*2
         }
+        // Update multiple display on index.html
         this.updateAffichageMultiple = function () {
             document.querySelector('#hMultiplier').innerHTML(this.level)
         }
